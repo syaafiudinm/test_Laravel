c2cbb4533166f28b784c317b691414320b5a658d
syaafiudinm <altafpasallo12@gmail.com>
2024-01-31 21:51:16 +0800

    fakert test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +17,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $faker = Faker::create('id_ID');
+
+        for($i = 1; $i <= 5; $i++){
+            DB::table('students')->insert([
+                'name' => $faker->name,
+                'score' => $faker->numberBetween(0,100)
+            ]);
+        }
     }
 }
