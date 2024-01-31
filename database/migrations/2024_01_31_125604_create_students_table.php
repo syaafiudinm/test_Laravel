43fa931e408e03d05e8cb7597b212bf4f8c5902a
syaafiudinm <altafpasallo12@gmail.com>
2024-01-31 21:10:47 +0800

    migration test

--- database/migrations/2024_01_31_125604_create_students_table.php
+++ database/migrations/2024_01_31_125604_create_students_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('score');
             $table->timestamps();
         });
     }
